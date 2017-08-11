afbeed89070b45379faef082c7205fd6161af529
Vibhatha Abeykoon <vibhatha@gmail.com>
2017-08-11 12:24:38 -0400

    fix:dev: ExpPCA code modified for more dynamic inputs

--- SparkMlibBenchmark/src/main/scala/edu/indiana/ise/spidal/pca/ExpPCA.scala
+++ SparkMlibBenchmark/src/main/scala/edu/indiana/ise/spidal/pca/ExpPCA.scala
@@ -1,5 +1,6 @@
 package edu.indiana.ise.spidal.pca
 
+import edu.indiana.ise.spidal.util.WriteText
 import org.apache.spark.{SparkConf, SparkContext}
 import org.apache.spark.mllib.feature.PCA
 import org.apache.spark.mllib.linalg.Vectors
@@ -10,26 +11,28 @@ import org.apache.spark.mllib.regression.{LabeledPoint, LinearRegressionWithSGD}
 object ExpPCA {
   def main(args: Array[String]) {
 
-    if(args.length!=1){
+    if(args.length!=3){
       ///home/vibhatha/ds/data/pca/matrix4
       println("Incorrect Argument format")
-      println("sh exp.sh /home/data/matrix0")
+      println("sh exp.sh /home/data/matrix0 /home/output/matrix1")
     }else{
       val filename=args(0)
+      val threads = args(1).toInt
+      val output = args(2)
       println("Spark PCA")
       val conf = new SparkConf().setAppName("My App")
       val sc = new SparkContext(conf)
-      val start_time = System.currentTimeMillis()	      
-	val data = sc.textFile("file:"+filename).map { line =>
+      val data = sc.textFile("file:"+filename).map { line =>
         val parts = line.split(',')
         LabeledPoint(parts(0).toDouble, Vectors.dense(parts(1).split(' ').map(_.toDouble)))
       }.cache()
 
-      val splits = data.randomSplit(Array(0.8, 0.2), seed = 11L)
+      val splits = data.randomSplit(Array(0.99, 0.01), seed = 11L)
       val training = splits(0).cache()
       val test = splits(1)
-      //val start_time = System.currentTimeMillis();
-      val pca = new PCA(training.first().features.size / 2).fit(data.map(_.features))
+      val start_time = System.currentTimeMillis();
+      val principalComponents=training.first().features.size / 2
+      val pca = new PCA(principalComponents).fit(data.map(_.features))
       val training_pca = training.map(p => p.copy(features = pca.transform(p.features)))
       val end_time = System.currentTimeMillis();
       val elapsed_time = (end_time - start_time)/1000.0
@@ -42,6 +45,14 @@ object ExpPCA {
       println("======================================")
       println("Time Elapsed : "+elapsed_time+" s")
       println("======================================")
+
+      val outputData = "Matrix : "+filename+"\n"
+      val itr = "Principal Components : "+principalComponents.toString+"\n"
+      val thd = "Threads : "+threads.toString+"\n"
+      val eta = "Execution Time : "+elapsed_time.toString+"\n"
+      val final_record = outputData+itr+thd+eta
+      WriteText.saveFile(output, final_record);
+
     }
 
   }

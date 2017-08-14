e694c63bbb0a8a11b1883a7d0c3fda695e363ff2
Vibhatha Abeykoon <vibhatha@gmail.com>
2017-08-14 16:53:28 -0400

    fix:dev: PCA data loading time calculation

---
+++ SparkMlibBenchmark/scripts/gen_svm_bulk.sh
@@ -0,0 +1,5 @@
+sh scripts/gen_svm.sh 100 100000 100 data/libsvm/mat_100000_100 10
+sh scripts/gen_svm.sh 100 200000 100 data/libsvm/mat_200000_100 10
+sh scripts/gen_svm.sh 100 300000 100 data/libsvm/mat_300000_100 10
+sh scripts/gen_svm.sh 100 400000 100 data/libsvm/mat_400000_100 10 
+sh scripts/gen_svm.sh 100 500000 100 data/libsvm/mat_500000_100 10
